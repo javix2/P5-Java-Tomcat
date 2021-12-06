74d2c8add036527c1cf2b23ff248d6e9a6dfdd78
javix2 <91162997+javix2@users.noreply.github.com>
2021-12-06 02:17:46 -0300

    Add files via upload

---
+++ java-Tomcat/Cliente/build/generated/jax-wsCache/ServicioWeb/paqueteservicio/SerieFibonacci.java
@@ -0,0 +1,52 @@
+
+package paqueteservicio;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>Clase Java para serieFibonacci complex type.
+ * 
+ * <p>El siguiente fragmento de esquema especifica el contenido que se espera que haya en esta clase.
+ * 
+ * <pre>
+ * &lt;complexType name="serieFibonacci"&gt;
+ *   &lt;complexContent&gt;
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType"&gt;
+ *       &lt;sequence&gt;
+ *         &lt;element name="numero" type="{http://www.w3.org/2001/XMLSchema}int"/&gt;
+ *       &lt;/sequence&gt;
+ *     &lt;/restriction&gt;
+ *   &lt;/complexContent&gt;
+ * &lt;/complexType&gt;
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "serieFibonacci", propOrder = {
+    "numero"
+})
+public class SerieFibonacci {
+
+    protected int numero;
+
+    /**
+     * Obtiene el valor de la propiedad numero.
+     * 
+     */
+    public int getNumero() {
+        return numero;
+    }
+
+    /**
+     * Define el valor de la propiedad numero.
+     * 
+     */
+    public void setNumero(int value) {
+        this.numero = value;
+    }
+
+}
